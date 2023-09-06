a4beb8b3d1bf2b5adca696fe7da34241194f71a4
cheemalove592-nnoMHx <cheemalove592@gmail.com>
2023-09-05 23:25:45 -0400

    its my first trial

---
+++ PyBank/main.py
@@ -0,0 +1,63 @@
+import os
+import csv
+
+budgetcsv= os.path.join("Resources", "budget_data.csv")
+
+with open(budgetcsv,'r') as csvfile:
+    csvreader=csv.reader(csvfile, delimiter=",")
+    csvheader=next(csvreader)
+    count=0
+    total_sum=0
+    changes=0
+    previous_entry=0
+    change_list=[]
+    Max_num=99999999999
+    Min_num=0
+    greatest_Inc=["",0]
+    greatest_dec=["",0]
+
+
+    for row in csvreader:
+        #count number of months
+        count+=1
+        #total sum
+        total_sum= total_sum+int(row[1])
+        #calculate the difference and add to list
+        changes=int(row[1])-previous_entry
+        previous_entry=int(row[1])
+        change_list.append(changes)
+        
+        # calculate the greatest decrease in profit
+
+        if changes< Max_num:
+            greatest_dec[0]=row[0]
+            greatest_dec[1]=changes
+            Max_num=changes
+        # calculate the greatest increase in profit
+        if changes> Min_num:
+            greatest_Inc[0]=row[0]
+            greatest_Inc[1]=changes
+            Min_num=changes
+
+
+        
+
+
+    #print(change_list)
+    #Display months
+    print(f'Total months: {count}')
+    #Display total sum
+    print(f'Total: ${total_sum}')
+    #Calculate and print average
+    Average= float((sum(change_list))/ len(change_list))
+    print(f'Average Change: ${round(Average,2)}')
+    #print greatest increase
+    print(f'Greatest Increase in Profits: {greatest_Inc[0]} (${ greatest_Inc[1]})')
+    #print greatest decrease
+    print(f'Greatest Decrease in Profits: {greatest_dec[0]} (${ greatest_dec[1]})')
+
+
+
+
+
+
