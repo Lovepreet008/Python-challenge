b538e20991e416a3de774f38fd0289db8301bfe9
cheemalove592-nnoMHx <cheemalove592@gmail.com>
2023-09-06 20:29:50 -0400

    Final code submission for PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,13 +8,13 @@ with open(budgetcsv,'r') as csvfile:
     csvheader=next(csvreader)
     count=0
     total_sum=0
-    changes=0
     previous_entry=0
     change_list=[]
     Max_num=99999999999
     Min_num=0
-    greatest_Inc=["",0]
     greatest_dec=["",0]
+    greatest_Inc=["",0]
+    
 
 
     for row in csvreader:
@@ -23,38 +23,48 @@ with open(budgetcsv,'r') as csvfile:
         #total sum
         total_sum= total_sum+int(row[1])
         #calculate the difference and add to list
-        changes=int(row[1])-previous_entry
-        previous_entry=int(row[1])
-        change_list.append(changes)
+        if previous_entry!=0:
+            changes=int(row[1])-previous_entry
+            
+            change_list.append(changes)
         
         # calculate the greatest decrease in profit
 
-        if changes< Max_num:
-            greatest_dec[0]=row[0]
-            greatest_dec[1]=changes
-            Max_num=changes
-        # calculate the greatest increase in profit
-        if changes> Min_num:
-            greatest_Inc[0]=row[0]
-            greatest_Inc[1]=changes
-            Min_num=changes
+            if changes< Max_num:
+                greatest_dec[0]=row[0]
+                greatest_dec[1]=changes
+                Max_num=changes
+            # calculate the greatest increase in profit
+            if changes> Min_num:
+                greatest_Inc[0]=row[0]
+                greatest_Inc[1]=changes
+                Min_num=changes
+        previous_entry=int(row[1])
+
+    
 
 
-        
 
 
-    #print(change_list)
+    print("Finacial Analysis")
+    print("_____________________________")
+    print("                             ")
     #Display months
     print(f'Total months: {count}')
+    print("                             ")
     #Display total sum
     print(f'Total: ${total_sum}')
+    print("                             ")
     #Calculate and print average
     Average= float((sum(change_list))/ len(change_list))
     print(f'Average Change: ${round(Average,2)}')
+    print("                             ")
     #print greatest increase
     print(f'Greatest Increase in Profits: {greatest_Inc[0]} (${ greatest_Inc[1]})')
+    print("                             ")
     #print greatest decrease
     print(f'Greatest Decrease in Profits: {greatest_dec[0]} (${ greatest_dec[1]})')
+    #$-8311.11
 
 
 
